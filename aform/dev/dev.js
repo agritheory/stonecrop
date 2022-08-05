c47da1d0ed6d9dbe53de2cf5b10314f8740f6733
Tyler Matteson <tyler@agritheory.com>
2022-08-05 10:21:22 -0400

    chore: update some dependencies

--- aform/dev/dev.js
+++ aform/dev/dev.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import Dev from './Dev.vue'
-import atable from '@sedum/atable'
+// import atable from '@sedum/atable'
 import aform from './../src/'
 
 let app = createApp(Dev)
 
-app.use(atable)
+// app.use(atable)
 app.use(aform)
 
 app.mount('#app')
