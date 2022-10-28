9a80fef2386ecf98c5290276e6377d0d00f175b4
Rohan Bansal <rohan@parsimony.com>
2022-10-28 12:24:16 +0530

    fix: separate styling from the script

--- atable/src/components/AExpansionRow.vue
+++ atable/src/components/AExpansionRow.vue
@@ -1,17 +1,23 @@
 <template>
-	<tr v-bind="$attrs" ref="rowEl" :tabindex="tabIndex" :style="rowStyle">
-		<td id="row-index" :tabIndex="-1" :style="listExpansionStyle" @click="toggleRowExpand(rowIndex)">
+	<tr v-bind="$attrs" ref="rowEl" :tabindex="tabIndex" class="expandable-row">
+		<td id="row-index" :tabIndex="-1" @click="tableData.toggleRowExpand(rowIndex)">
 			{{ getRowExpandSymbol() }}
 		</td>
-		<slot name="indexCell" />
 		<slot />
 	</tr>
-	<div v-if="tableData.display[props.rowIndex].expanded" class="expand">Hello</div>
+	<div
+		v-if="tableData.display[props.rowIndex].expanded"
+		ref="rowExpanded"
+		:tabindex="tabIndex"
+		class="expandable-row-content">
+		<slot name="expanded" />
+	</div>
 </template>
 
 <script setup lang="ts">
 import { TableRow } from 'types'
-import { CSSProperties, inject, ref } from 'vue'
+import { inject, ref } from 'vue'
+
 import { useKeyboardNav } from '@agritheory/utilities'
 
 import TableDataStore from '.'
@@ -22,7 +28,9 @@ const props = withDefaults(
 		rowIndex: number
 		tableid: string
 		tabIndex?: number
-		addNavigation?: object
+		addNavigation?: {
+			[key: string]: (ev: KeyboardEvent) => any
+		}
 	}>(),
 	{
 		tabIndex: -1,
@@ -31,29 +39,13 @@ const props = withDefaults(
 
 const tableData = inject<TableDataStore>(props.tableid)
 const rowEl = ref<HTMLTableRowElement>(null)
-
-const listExpansionStyle: CSSProperties = {
-	color: 'var(--header-text-color)',
-	fontWeight: 'bold',
-	textAlign: 'center',
-	userSelect: 'none',
-	width: '2ch',
-}
-
-const rowStyle: CSSProperties = {
-	borderTop: '1px solid var(--row-border-color)',
-	height: 'var(--atable-row-height)',
-}
+const rowExpanded = ref<HTMLDivElement>(null)
 
 const getRowExpandSymbol = () => {
 	return tableData.display[props.rowIndex].expanded ? '▼' : '►'
 }
 
-const toggleRowExpand = (rowIndex: number) => {
-	tableData.toggleRowExpand(rowIndex)
-}
-
-if (props.addNavigation) {
+if (props.addNavigation !== undefined) {
 	useKeyboardNav([
 		{
 			selectors: rowEl,
@@ -62,3 +54,24 @@ if (props.addNavigation) {
 	])
 }
 </script>
+
+<style scoped>
+#row-index {
+	color: var(--header-text-color);
+	font-weight: bold;
+	text-align: center;
+	user-select: none;
+	width: 2ch;
+}
+
+.expandable-row {
+	border-top: 1px solid var(--row-border-color);
+	height: var(--atable-row-height);
+}
+
+.expandable-row-content {
+	border-bottom: 1px solid var(--row-border-color);
+	border-top: 1px solid var(--row-border-color);
+	padding: 0.5rem;
+}
+</style>
