f22d2300c9a0f387d96d583def22f6a1b5f0d933
Tyler Matteson <tyler@agritheory.com>
2022-11-28 10:15:33 -0500

    wip: component registry

--- stonecrop/stories/components/Home.vue
+++ stonecrop/stories/components/Home.vue
@@ -1,10 +1,12 @@
 <template>
 	<div id="home">
 		<pre>{{ state }}</pre>
+		<Fieldtype></Fieldtype>
 	</div>
 </template>
 <script setup lang="ts">
 import { useStonecrop } from '@/composable'
+import Fieldtype from './Fieldtype.vue'
 
 let state = useStonecrop()
 </script>
