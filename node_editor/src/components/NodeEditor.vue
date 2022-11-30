7aefe27ea6c5f04dab9769214a6cf734b5ca5b06
John Kelley <johnhkelley1@gmail.com>
2022-11-30 14:08:19 -0700

    baklava v2

--- node_editor/src/components/NodeEditor.vue
+++ node_editor/src/components/NodeEditor.vue
@@ -6,9 +6,49 @@
 <script>
 import { defineComponent } from 'vue'
 import { EditorComponent, useBaklava } from '@baklavajs/renderer-vue'
-import '@baklavajs/themes/dist/syrup-dark.css'
-import WorkflowStateNode from '@/nodes/WorkflowStateNode'
+//import { NodeBuilder, Editor } from "@baklavajs/core";
+import * as mod from '@baklavajs/core'
 
+import { NodeBuilder, Editor } from '@baklavajs/core'
+import { ViewPlugin } from '@baklavajs/renderer-vue'
+//import { OptionPlugin } from "@baklavajs/plugin-options-vue";
+import { Engine } from '@baklavajs/engine'
+
+console.log('mode', mod)
+
+import '@baklavajs/themes/dist/syrup-dark.css'
+
+const mathNode = new NodeBuilder('MathNode')
+	.setName('Math')
+	.addInputInterface('Number 1', 'NumberOption', 1)
+	.addInputInterface('Number 2', 'NumberOption', 10)
+	.addOption('Operation', 'SelectOption', 'Add', undefined, {
+		items: ['Add', 'Subtract'],
+	})
+	.addOutputInterface('Output')
+	.onCalculate(n => {
+		const n1 = n.getInterface('Number 1').value
+		const n2 = n.getInterface('Number 2').value
+		const operation = n.getOptionValue('Operation')
+		let result
+		if (operation === 'Add') {
+			result = n1 + n2
+		} else if (operation === 'Subtract') {
+			result = n1 - n2
+		}
+		n.getInterface('Output').value = result
+	})
+	.build()
+// const TestNode =  defineNode({
+//     type: "WorkflowStateNode",
+//     inputs: {
+//
+//     },
+//     outputs: {
+//
+//     },
+// })
+///console.log("mod", mod);
 export default defineComponent({
 	name: 'NodeEditor',
 	components: {
@@ -16,7 +56,8 @@ export default defineComponent({
 	},
 	setup() {
 		const baklava = useBaklava()
-		//baklava.editor.registerNodeType(WorkflowStateNode);
+		//baklava.editor.addNode(mathNode);
+		baklava.editor.registerNodeType('My Node', mathNode)
 		console.log('BAK', baklava)
 		return { baklava }
 	},
--- node_editor/src/nodes/WorkflowStateNode.ts
+++ node_editor/src/nodes/WorkflowStateNode.ts
@@ -1,23 +1,24 @@
-import { NodeBuilder } from '@baklavajs/core'
-
-export default new NodeBuilder('WorkflowStateNode')
-	.setName('WorkflowState')
-	.addInputInterface('Number 1', 'NumberOption', 1)
-	.addInputInterface('Number 2', 'NumberOption', 10)
-	.addOption('Operation', 'SelectOption', 'Add', undefined, {
-		items: ['Add', 'Subtract'],
-	})
-	.addOutputInterface('Output')
-	.onCalculate(n => {
-		const n1 = n.getInterface('Number 1').value
-		const n2 = n.getInterface('Number 2').value
-		const operation = n.getOptionValue('Operation')
-		let result
-		if (operation === 'Add') {
-			result = n1 + n2
-		} else if (operation === 'Subtract') {
-			result = n1 - n2
-		}
-		n.getInterface('Output').value = result
-	})
-	.build()
+// import { defineNode, NodeInterface, NumberInterface, SelectInterface } from "baklavajs";
+//
+// export default defineNode({
+//     type: "WorkflowStateNode",
+//     inputs: {
+//         number1: () => new NumberInterface("Number", 1),
+//         number2: () => new NumberInterface("Number", 10),
+//         operation: () => new SelectInterface("Operation", "Add", ["Add", "Subtract"]).setPort(false),
+//     },
+//     outputs: {
+//         output: () => new NodeInterface("Output", 0),
+//     },
+//     calculate({ number1, number2, operation }) {
+//         let output: number;
+//         if (operation === "Add") {
+//             output = number1 + number2;
+//         } else if (operation === "Subtract") {
+//             output = number1 - number2;
+//         } else {
+//             throw new Error("Unknown operation: " + operation);
+//         }
+//         return { output };
+//     },
+// });
