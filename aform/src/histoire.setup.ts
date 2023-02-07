66a4d08468ea698ce6114b4f3d94633487a8ce45
Rohan Bansal <rohan@parsimony.com>
2023-02-07 12:52:04 +0530

    fix: move checkbox to composition api

--- aform/src/histoire.setup.ts
+++ aform/src/histoire.setup.ts
@@ -1,16 +1,17 @@
 import { defineSetupVue3 } from '@histoire/plugin-vue'
 
-import AForm from '@/components/AForm.vue'
+import ACheckbox from '@/components/form/ACheckbox.vue'
 import AFieldset from '@/components/form/AFieldset.vue'
+import AForm from '@/components/AForm.vue'
 import ANumericInput from '@/components/form/ANumericInput.vue'
 import ATextInput from '@/components/form/ATextInput.vue'
-import ACheckbox from '@/components/form/ACheckbox.vue'
 
 import { ATable, ATableHeader, ATableModal } from '@agritheory/atable'
 import '@agritheory/atable/styles'
 
 export const setupVue3 = defineSetupVue3(({ app }) => {
 	// TODO: (typing) add typing for ATable components
+	app.component('ACheckbox', ACheckbox)
 	app.component('AFieldset', AFieldset)
 	app.component('AForm', AForm)
 	app.component('ANumericInput', ANumericInput)
@@ -18,5 +19,4 @@ export const setupVue3 = defineSetupVue3(({ app }) => {
 	app.component('ATableHeader', ATableHeader)
 	app.component('ATableModal', ATableModal)
 	app.component('ATextInput', ATextInput)
-	app.component('ACheckbox', ACheckbox)
 })
