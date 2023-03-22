06c0d5661ca0211998113314024cbb870de89eac
Rohan Bansal <rohan@parsimony.com>
2023-03-22 11:49:34 +0530

    ci: add test runner for graphql client

--- graphql_client/vite.config.ts
+++ graphql_client/vite.config.ts
@@ -1,5 +1,7 @@
+/// <reference types="vitest" />
+
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 const projectRootDir = resolve(__dirname)
 
@@ -16,5 +18,37 @@ export default defineConfig({
 			name: '@agritheory/graphql-client',
 		},
 		rollupOptions: {},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		coverage: {
+			lines: 50,
+			branches: 50,
+			functions: 50,
+			statements: 50,
+			// required for Github Actions CI
+			reporter: ['text', 'json-summary', 'json'],
+			reportsDirectory: './coverage',
+			exclude: [
+				'coverage/**',
+				'dist/**',
+				'stories/**',
+				'types/**',
+				'packages/*/test{,s}/**',
+				'**/*.d.ts',
+				'**/*.setup.ts',
+				'cypress/**',
+				'test{,s}/**',
+				'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
+				'**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
+				'**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
+				'**/__tests__/**',
+				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+				'**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
+			],
+			all: true,
+			skipFull: true,
+		},
 	},
 })
