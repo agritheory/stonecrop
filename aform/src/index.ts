2d7c4eacdf1d96b27249823d50a7da1acf5436cd
Rohan Bansal <rohan@parsimony.com>
2023-03-30 12:19:58 +0530

    feat: add AQuantity component

--- aform/src/index.ts
+++ aform/src/index.ts
@@ -6,6 +6,7 @@ import ADate from '@/components/form/ADate.vue'
 import AFieldset from '@/components/form/AFieldset.vue'
 import AForm from '@/components/AForm.vue'
 import ANumericInput from '@/components/form/ANumericInput.vue'
+import AQuantity from '@/components/form/AQuantity.vue'
 import ATextInput from '@/components/form/ATextInput.vue'
 // import { ACurrency } from '@/components/form/ACurrency.vue'
 // import { AQuantity } from '@/components/form/AQuantity.vue'
@@ -17,9 +18,10 @@ function install(app: App /* options */) {
 	app.component('AFieldset', AFieldset)
 	app.component('AForm', AForm)
 	app.component('ANumericInput', ANumericInput)
+	app.component('AQuantity', AQuantity)
 	app.component('ATextInput', ATextInput)
 	// app.component('ACurrency', ACurrency)
 	// app.component('AQuantity', AQuantity)
 }
 
-export { ACheckbox, AComboBox, ADate, AFieldset, AForm, ANumericInput, ATextInput, install }
+export { ACheckbox, AComboBox, ADate, AFieldset, AForm, ANumericInput, AQuantity, ATextInput, install }
