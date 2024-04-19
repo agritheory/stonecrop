5d97ad04fe6c3bb02a5bdc991285323b11e8accf
Rohan Bansal <rohan@agritheory.dev>
2024-04-19 14:16:28 +0530

    fix: add login story

--- aform/src/components/utilities/Login.vue
+++ aform/src/components/utilities/Login.vue
@@ -1,29 +1,10 @@
 <template>
 	<div class="login-container">
-		<span>Login</span>
-
-		<div class="entity-container">
-			<div id="entity">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					class="mr-2 h-6 w-6">
-					<path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-				</svg>
-				{{ entityName }}
-			</div>
-		</div>
-
 		<div>
 			<div class="account-container">
 				<div class="account-header">
-					<h1 id="account-title">Create an account</h1>
-					<p id="account-subtitle">Enter your email below to create your account</p>
+					<h1 id="account-title">{{ headerTitle }}</h1>
+					<p id="account-subtitle">{{ headerSubtitle }}</p>
 				</div>
 
 				<form @submit="onSubmit">
@@ -32,6 +13,7 @@
 							<label id="login-email" for="email">Email</label>
 							<input
 								id="email"
+								name="email"
 								placeholder="name@example.com"
 								type="email"
 								auto-capitalize="none"
@@ -39,15 +21,21 @@
 								auto-correct="off"
 								:disabled="isLoading" />
 						</div>
+
+						<div class="login-form-password">
+							<label id="login-password" for="password">Password</label>
+							<input id="password" name="password" type="password" :disabled="isLoading" />
+						</div>
+
 						<button :disabled="isLoading">
-							<span v-if="isLoading" class="login-form-button">Sign In with Email</span>
+							<span id="login-form-button">Login</span>
 						</button>
 					</div>
 				</form>
 
-				<p id="terms">
-					By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
-				</p>
+				<button>
+					<span id="forgot-password-button">Forgot password?</span>
+				</button>
 			</div>
 		</div>
 	</div>
@@ -56,9 +44,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{
-	entityName: string
-}>()
+withDefaults(
+	defineProps<{
+		headerTitle?: string
+		headerSubtitle?: string
+	}>(),
+	{
+		headerTitle: 'Login',
+		headerSubtitle: 'Enter your email and password to login',
+	}
+)
 
 const isLoading = ref(false)
 
@@ -78,25 +73,6 @@ function onSubmit(event: Event) {
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-}
-
-.entity-container {
-	height: 100%;
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	padding: 2.5rem;
-	color: rgb(255 255 255);
-}
-
-#entity {
-	position: relative;
-	z-index: 20;
-	display: flex;
-	align-items: center;
-	font-size: 1.125rem;
-	line-height: 1.75rem;
-	font-weight: 500;
 }
 
 .account-container {
@@ -133,12 +109,14 @@ function onSubmit(event: Event) {
 	gap: 0.5rem;
 }
 
-.login-form-email {
+.login-form-email,
+.login-form-password {
 	display: grid;
 	gap: 0.25rem;
 }
 
-#login-email {
+#login-email,
+#login-password {
 	position: absolute;
 	width: 1px;
 	height: 1px;
@@ -150,7 +128,7 @@ function onSubmit(event: Event) {
 	border-width: 0;
 }
 
-.login-form-button {
+#login-form-button {
 	margin-right: 0.5rem;
 	height: 1rem;
 	width: 1rem;
@@ -167,11 +145,7 @@ function onSubmit(event: Event) {
 	}
 }
 
-#terms {
-	text-align: center;
-	font-size: 0.875rem;
-	line-height: 1.25rem;
-	padding-left: 2rem;
-	padding-right: 2rem;
+#forgot-password-button {
+	/* TODO: add styling for button */
 }
 </style>
