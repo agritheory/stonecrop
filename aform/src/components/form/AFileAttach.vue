a76e43af59c421c43453d12934e99ca46bf63037
Devarsh Bhatt <bhatt.devarsh17@gmail.com>
2024-05-06 14:54:43 +0530

    [aForm, atable] upgrade vueuse for file attach

--- aform/src/components/form/AFileAttach.vue
+++ aform/src/components/form/AFileAttach.vue
@@ -1,31 +1,36 @@
 <template>
-  <button type="button" @click="open()">
-    {{ props.label }}
-  </button> &nbsp;
-  <button type="button" :disabled="!files" @click="reset()">
-    Reset
-  </button>
+	<button type="button" @click="open()">
+		{{ props.label }}
+	</button>
+	&nbsp;
+	<button type="button" :disabled="!files" @click="reset()">Reset</button>
 
-  <template v-if="files">
-    <div>
-      <p>You have selected: <b>{{ `${files.length} ${files.length === 1 ? 'file' : 'files'}` }}</b></p>
-      <li v-for="file of files" :key="file.name">
-        {{ file.name }}
-      </li>
-    </div>
-  </template>
+	<template v-if="files">
+		<div>
+			<p>
+				You have selected: <b>{{ `${files.length} ${files.length === 1 ? 'file' : 'files'}` }}</b>
+			</p>
+			<li v-for="file of files" :key="file.name">
+				{{ file.name }}
+			</li>
+		</div>
+	</template>
 </template>
 
 <script setup lang="ts">
 import { useFileDialog } from '@vueuse/core'
-import { SchemaTypes } from 'types/index';
+import { SchemaTypes } from 'types/index'
 
 const props = defineProps<{
-  schema: SchemaTypes[]
-  label: string
-  collapsible?: boolean
-  data?: any
+	schema: SchemaTypes[]
+	label: string
+	collapsible?: boolean
+	data?: any
 }>()
 
-const { files, open, reset } = useFileDialog()
+const { files, open, reset, onChange } = useFileDialog()
+
+onChange(files => {
+	return files
+})
 </script>
