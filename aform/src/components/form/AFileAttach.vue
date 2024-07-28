c2ba5a739ac98660413eb933bfdf6bd1c26cec3c
Curt Rabinak <curt@crabinak.com>
2024-07-28 13:33:31 -0500

    styled file attach componet and updated form fields to use CSS grid instead of flexbox

--- aform/src/components/form/AFileAttach.vue
+++ aform/src/components/form/AFileAttach.vue
@@ -1,20 +1,22 @@
 <template>
-	<button type="button" @click="open()">
-		{{ props.label }}
-	</button>
-	&nbsp;
-	<button type="button" :disabled="!files" @click="reset()">Reset</button>
-
-	<template v-if="files">
-		<div>
-			<p>
-				You have selected: <b>{{ `${files.length} ${files.length === 1 ? 'file' : 'files'}` }}</b>
-			</p>
-			<li v-for="file of files" :key="file.name">
-				{{ file.name }}
-			</li>
-		</div>
-	</template>
+	<div class="aform__form-element aform__file-attach aform__grid--full">
+		
+		
+		<template v-if="files">
+			<div class="aform__file-attach-feedback">
+				<p>
+					You have selected: <b>{{ `${files.length} ${files.length === 1 ? 'file' : 'files'}` }}</b>
+				</p>
+				<li v-for="file of files" :key="file.name">
+					{{ file.name }}
+				</li>
+			</div>
+		</template>
+		<button type="button" @click="open()" class="aform__form-btn">
+			{{ props.label }}
+		</button>
+		<button type="button" :disabled="!files" @click="reset()" class="aform__form-btn">Reset</button>
+	</div>
 </template>
 
 <script setup lang="ts">
