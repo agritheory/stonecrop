2d68e1c7149bd788ca7230d37cd707f9503a13af
Rohan Bansal <rohan@agritheory.dev>
2024-09-13 14:36:55 +0530

    fix: update changelogs

--- beam/src/index.ts
+++ beam/src/index.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
 import { App } from 'vue'
 
 import ActionFooter from '@/components/ActionFooter.vue'
