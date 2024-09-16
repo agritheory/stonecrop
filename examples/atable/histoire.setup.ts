033fca6b47918818b49c8a3f46a6dc233629229b
Rohan <Alchez@users.noreply.github.com>
2024-09-16 17:24:43 +0530

    fix: isolate story-specific Beam styles (#151)
    * fix: isolate story-specific Beam styles
    
    * fix: use plugin installation for histoire setup
    
    * fix: add changelogs
    
    ---------
    
    Co-authored-by: Rohan Bansal <rohan@agritheory.dev>

--- examples/atable/histoire.setup.ts
+++ examples/atable/histoire.setup.ts
@@ -1,18 +1,8 @@
 import { defineSetupVue3 } from '@histoire/plugin-vue'
-
-import { ADate, AFileAttach, AForm, ANumericInput, ATextInput } from '@stonecrop/aform'
-import { ACell, AExpansionRow, ARow, ATable, ATableHeader, ATableModal } from '@stonecrop/atable'
+import { install as AForm } from '@stonecrop/aform'
+import { install as ATable } from '@stonecrop/atable'
 
 export const setupVue3 = defineSetupVue3(({ app }) => {
-	app.component('ACell', ACell)
-	app.component('ADate', ADate)
-	app.component('AExpansionRow', AExpansionRow)
-	app.component('AForm', AForm)
-	app.component('ANumericInput', ANumericInput)
-	app.component('ARow', ARow)
-	app.component('ATable', ATable)
-	app.component('ATableHeader', ATableHeader)
-	app.component('ATableModal', ATableModal)
-	app.component('ATextInput', ATextInput)
-	app.component('AFileAttach', AFileAttach)
+	app.use(AForm)
+	app.use(ATable)
 })
--- examples/beam/histoire.setup.ts
+++ examples/beam/histoire.setup.ts
@@ -2,6 +2,8 @@ import { defineSetupVue3 } from '@histoire/plugin-vue'
 import { install as Beam } from '@stonecrop/beam'
 import PortalVue from 'portal-vue'
 
+import './overrides.css'
+
 export const setupVue3 = defineSetupVue3(({ app }) => {
 	app.use(PortalVue)
 	app.use(Beam)
