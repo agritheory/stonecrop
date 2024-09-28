cfa17dbc84a24ff0b3629a1ec58b1edd0d18941e
curt <curt@crabinak.com>
2024-09-28 17:43:44 -0500

    added responsive styling and shipping arrow

--- beam/src/components/BeamMetadata.vue
+++ beam/src/components/BeamMetadata.vue
@@ -3,28 +3,44 @@
 		<div class="beam_metadata_content">
 			<div class="beam_metadata_header beam_metadata--two-column">
 				<h1 class="beam_metadata_header-order-num">
-					WO#2024-01-00001 <span class="beam_metadata--normal">Ambrosia Pie</span>
+					{{ order.orderNumber }} <span class="beam_metadata--normal">{{ order.product }}</span>
 				</h1>
-				<div class="alert beam_metadata_count">
-					<p>0/2</p>
+				<div :class="{ alert: !order.complete }" class="beam_metadata_count">
+					<p>{{ order.quantity }}/{{ order.total }}</p>
 				</div>
 			</div>
 			<div class="beam_metadata_block">
-				<p class="beam_metadata_heading">Status: <span class="alert beam_metadata--normal">In Progress</span></p>
+				<p class="beam_metadata_heading">
+					Status:
+					<span :class="{ alert: !order.complete }" class="beam_metadata--normal">{{
+						order.complete ? 'Complete' : 'In Progress'
+					}}</span>
+				</p>
 			</div>
 			<div class="beam_metadata_shipping beam_metadata_block">
 				<div class="beam_metadata_source">
 					<p class="beam_metadata_heading">Source</p>
-					<p class="beam_metadata--single-line">Packing Warehouse Inc.</p>
-					<p class="beam_metadata--single-line">1432 W. Parkway Ave.</p>
-					<p class="beam_metadata--single-line">Chicago, IL 60617</p>
+					<p>
+						Packing Warehouse Inc.<br />
+						1432 W. Parkway Ave.<br />
+						Chicago, IL 60617
+					</p>
 				</div>
-				<div class="beam_metadata_arrow"></div>
+				<div class="beam_metadata_arrow">
+					<div class="beam_metadata_arrow-body"></div>
+					<div class="beam_metadata_arrow-head">
+						<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6.74 7.78">
+							<polygon points="6.74 3.89 0 0 0 7.78 6.74 3.89" style="fill: #c4c4c4" />
+						</svg>
+					</div>
+				</div>
 				<div class="beam_metadata_source">
 					<p class="beam_metadata_heading">Receiving</p>
-					<p class="beam_metadata--single-line">Packing Warehouse Inc.</p>
-					<p class="beam_metadata--single-line">1432 W. Parkway Ave.</p>
-					<p class="beam_metadata--single-line">Chicago, IL 60617</p>
+					<p>
+						Packing Warehouse Inc.<br />
+						1432 W. Parkway Ave.<br />
+						Chicago, IL 60617
+					</p>
 				</div>
 			</div>
 			<div class="beam_metadata_component_header beam_metadata_block">
@@ -33,10 +49,19 @@
 			</div>
 		</div>
 		<div class="beam_metadata_components">
-			<!--slot added here-->
 			<slot name="components"></slot>
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+	order: {
+		orderNumber: string
+		product: string
+		quantity: number
+		total: number
+		complete: boolean
+	}
+}>()
+</script>
