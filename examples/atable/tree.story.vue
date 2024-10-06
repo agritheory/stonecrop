78ad2bccaae1607fca64b0bfbd367cf182bdfe3f
Rohan Bansal <rohan@agritheory.dev>
2024-10-06 11:57:42 +0530

    fix: add story for pinned columns in tree

--- examples/atable/tree.story.vue
+++ examples/atable/tree.story.vue
@@ -1,6 +1,12 @@
 <template>
 	<Story title="tree">
-		<ATable v-model="coa.rows" :columns="coa.columns" :config="coa.config" />
+		<Variant title="default">
+			<ATable v-model="coa.rows" :columns="coa.columns" :config="coa.config" />
+		</Variant>
+
+		<Variant title="pinned columns">
+			<ATable v-model="pinned.rows" :columns="pinned.columns" :config="pinned.config" />
+		</Variant>
 	</Story>
 </template>
 
@@ -10,38 +16,79 @@ import { ref } from 'vue'
 
 import data from './sample_data/coa.json'
 
+const default_columns = [
+	{
+		label: 'Number',
+		name: 'account_number',
+		type: 'Data',
+		align: 'left',
+		edit: false,
+		width: '10ch',
+	},
+	{
+		label: 'Account',
+		name: 'account_title',
+		type: 'Data',
+		align: 'left',
+		edit: true,
+		width: '50ch',
+	},
+	{
+		label: 'Balance',
+		name: 'balance',
+		type: 'Data',
+		align: 'left',
+		edit: false,
+		width: '20ch',
+		format: (value: { title?: string; value?: any }) => {
+			return value.title
+		},
+		modalComponent: 'TestModalComponent',
+	},
+] as TableColumn[]
+
+const pinned_columns = [
+	{
+		label: 'Number',
+		name: 'account_number',
+		type: 'Data',
+		align: 'left',
+		edit: false,
+		width: '10ch',
+		pinned: false,
+	},
+	{
+		label: 'Account',
+		name: 'account_title',
+		type: 'Data',
+		align: 'left',
+		edit: true,
+		width: '50ch',
+		pinned: true,
+	},
+	{
+		label: 'Balance',
+		name: 'balance',
+		type: 'Data',
+		align: 'left',
+		edit: false,
+		width: '20ch',
+		format: (value: { title?: string; value?: any }) => {
+			return value.title
+		},
+		modalComponent: 'TestModalComponent',
+	},
+]
+
 const coa = ref({
 	rows: data as TableRow[],
-	columns: [
-		{
-			label: 'Number',
-			name: 'account_number',
-			type: 'Data',
-			align: 'left',
-			edit: false,
-			width: '10ch',
-		},
-		{
-			label: 'Account',
-			name: 'account_title',
-			type: 'Data',
-			align: 'left',
-			edit: true,
-			width: '50ch',
-		},
-		{
-			label: 'Balance',
-			name: 'balance',
-			type: 'Data',
-			align: 'left',
-			edit: false,
-			width: '20ch',
-			format: (value: { title?: string; value?: any }) => {
-				return value.title
-			},
-			modalComponent: 'TestModalComponent',
-		},
-	] as TableColumn[],
+	columns: default_columns,
+	config: { view: 'tree' } as TableConfig,
+})
+
+const pinned = ref({
+	rows: data as TableRow[],
+	columns: pinned_columns,
 	config: { view: 'tree' } as TableConfig,
 })
 </script>
