c8e0173bb9852defef814889177044bfe6c543b5
Lauty <38353324+lauty95@users.noreply.github.com>
2025-01-08 15:01:34 -0300

    feat: editing values

--- examples/beam/default.story.vue
+++ examples/beam/default.story.vue
@@ -34,7 +34,7 @@
 							<BeamHeading> WO#2024-01-00001 <span class="beam--normal">Ambrosia Pie</span> </BeamHeading>
 						</template>
 						<template #right>
-							<ItemCount denominator="20" model-value="5" />
+							<ItemCount :denominator="workOrder.total" v-model="workOrder.quantity" />
 						</template>
 					</SplitColumn>
 					<BeamProgress :complete="workOrder.complete" progress-message="In Transit" />
