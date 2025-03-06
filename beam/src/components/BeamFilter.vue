e3ef602fa9e50a83a653630b819ad7c050c63a94
Rohan <Alchez@users.noreply.github.com>
2025-03-06 22:48:21 +0530

    [beam] render filter menus over list content (#250)
    * fix: render filter menus over list content
    
    * fix: add changelogs
    
    * fix: add click outside directive for menus
    
    * style: lower padding in beam filter section
    
    * fix: don't reset selected value when filter visibilty is toggled
    
    * style: space out the dropdown menus
    
    * added gap between filter options. aligned filter options to left
    
    ---------
    
    Co-authored-by: Rohan Bansal <rohan@agritheory.dev>
    Co-authored-by: curt <curt@crabinak.com>

--- beam/src/components/BeamFilter.vue
+++ beam/src/components/BeamFilter.vue
@@ -1,66 +1,42 @@
 <template>
-	<div ref="beam-filters" class="beam_filters" :style="{ height: isOpen ? '100%' : headerHeight }">
-		<div ref="beam-filters-header" @click="toggle" class="beam_filters-heading">
-			<ToggleArrow :open="isOpen" />
+	<div class="beam_filters">
+		<div @click="isFilterExpanded = !isFilterExpanded" class="beam_filters-heading">
+			<ToggleArrow :open="isFilterExpanded" />
 			<BeamHeading>Filter</BeamHeading>
 		</div>
 
-		<div class="beam_filters-options">
-			<slot></slot>
+		<div v-show="isFilterExpanded" class="beam_filters-options">
+			<slot />
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, useTemplateRef } from 'vue'
+import { ref } from 'vue'
 
 defineSlots<{ default(): any }>()
 
-const header = useTemplateRef('beam-filters-header')
-const beamFilters = useTemplateRef('beam-filters')
-
-const isOpen = ref(false)
-const headerHeight = ref<string>()
-const totalHeight = ref<string>()
-
-const toggle = () => {
-	isOpen.value = !isOpen.value
-}
-
-onMounted(() => {
-	if (header.value && beamFilters.value) {
-		headerHeight.value = getTotalHeight(header.value)
-		totalHeight.value = getTotalHeight(beamFilters.value)
-		beamFilters.value.style.height = headerHeight.value
-	}
-})
-
-const getTotalHeight = (el: HTMLDivElement) => {
-	const height = el.getBoundingClientRect().height
-	const marginTop = parseInt(getComputedStyle(el).marginTop)
-	const marginBottom = parseInt(getComputedStyle(el).marginBottom)
-	return height + marginTop + marginBottom + 'px'
-}
+const isFilterExpanded = ref(false)
 </script>
 
 <style scoped>
 .beam_filters {
-	overflow: hidden;
-	box-sizing: border-box;
-	transition: all 0.2s ease-in-out;
+	background-color: var(--sc-primary-color);
 	border-bottom: 1px solid var(--sc-row-border-color);
-	background: white;
+	box-sizing: border-box;
+	min-height: 2em;
+	overflow: visible;
+	padding: 0.625rem;
+	transition: all 0.2s ease-in-out;
 }
 
 .beam_filters-heading {
-	background: var(--sc-primary-color);
+	align-items: center;
+	background-color: var(--sc-primary-color);
+	box-sizing: border-box;
 	cursor: pointer;
 	display: flex;
-	align-items: center;
-	padding-left: 1rem;
-	box-sizing: border-box;
 	font-size: 1rem;
-	padding: 0 2rem;
 
 	& > h1 {
 		font-size: 1rem;
@@ -70,11 +46,10 @@ const getTotalHeight = (el: HTMLDivElement) => {
 .beam_filters-options {
 	display: flex;
 	flex-direction: row;
-	column-gap: 1rem;
-	background: white;
-	box-sizing: border-box;
-	padding: 0 1rem;
+	justify-content: flex-start;
 	margin: 1rem 0;
+	padding: 0 1rem;
+	gap: 1rem;
 
 	@media (max-width: 479px) {
 		flex-direction: column;
