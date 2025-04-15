18ad88e2a16cd17bd7cefa2bef5ffa6b0503e673
Rohan <Alchez@users.noreply.github.com>
2025-04-15 15:01:42 +0530

    [atable] use specific key to fetch gantt bar's color (#256)

--- atable/src/components/AGanttCell.vue
+++ atable/src/components/AGanttCell.vue
@@ -66,8 +66,8 @@ const rightHandleRef = useTemplateRef('rightHandle')
 
 const { width: totalBarWidth } = useElementBounding(containerRef)
 const { left: barLeft, right: barRight } = useElementBounding(barRef)
-const currentStart = ref(start || 1)
-const currentEnd = ref(end || 4)
+const currentStart = ref(start || 0)
+const currentEnd = ref(end || colspan)
 const dragStartData = ref({ startX: 0, startPos: 0 })
 
 const pixelsPerColumn = computed(() => (colspan > 0 ? totalBarWidth.value / colspan : 0))
