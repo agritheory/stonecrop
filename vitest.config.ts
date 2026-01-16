85b6699ad38ce2607b4161e5c410f3cd00092b5c
Rohan Bansal <rohanbansal33@gmail.com>
2026-01-16 16:20:01 +0530

    test: add type testing for CASL functions

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		projects: ['aform/*/vite.config.ts', 'atable/*/vite.config.ts', 'beam/*/vite.config.ts', 'nuxt/*'],
+		projects: [
+			'aform/*/vite.config.ts',
+			'atable/*/vite.config.ts',
+			'beam/*/vite.config.ts',
+			'casl_middleware/*/vite.config.ts',
+			'nuxt/*',
+			'rockfoil/*/vite.config.ts',
+			'stonecrop/*/vite.config.ts',
+		],
 	},
 })
