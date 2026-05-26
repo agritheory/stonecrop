1f583d038a7ec91900ab70ea808797597d792f96
Rohan Bansal <rohanbansal33@gmail.com>
2026-05-26 12:24:34 +0530

    fix: add linting for schema

--- schema/src/converter/index.ts
+++ schema/src/converter/index.ts
@@ -10,7 +10,7 @@
 import { buildClientSchema, buildSchema, isObjectType, type GraphQLSchema } from 'graphql'
 
 import type { LinkDeclaration } from '../doctype'
-import { toSlug, pascalToSnake } from '../naming'
+import { toSlug } from '../naming'
 import type { IntrospectionSource, GraphQLConversionOptions, ConvertedGraphQLDoctype } from './types'
 import { defaultIsEntityType, defaultIsEntityField, classifyFieldType } from './heuristics'
 
@@ -137,7 +137,7 @@ export function convertGraphQLSchema(
 				if (field._isLink && typeof field.options === 'string' && field.cardinality) {
 					links[field.fieldname] = {
 						target: field.options,
-						cardinality: field.cardinality as LinkDeclaration['cardinality'],
+						cardinality: field.cardinality,
 					}
 					return false
 				}
