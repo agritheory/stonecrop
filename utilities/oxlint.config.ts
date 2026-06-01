41634121bbea6433eeb545c508fce52b2cd2b72a
Rohan Bansal <rohanbansal33@gmail.com>
2026-06-01 11:06:45 +0530

    chore: move utilities package from eslint to oxlint

--- utilities/oxlint.config.ts
+++ utilities/oxlint.config.ts
@@ -5,4 +5,10 @@ import root from '../oxlint.config.ts'
 export default defineConfig({
 	extends: [root],
 	plugins: ['typescript', 'unicorn', 'oxc', 'import', 'promise', 'vitest', 'vue'],
+	ignorePatterns: ['eslint.config.js'],
+	rules: {
+		'no-console': 'error',
+		'no-underscore-dangle': 'error',
+		'typescript/no-unsafe-type-assertion': 'error',
+	},
 })
