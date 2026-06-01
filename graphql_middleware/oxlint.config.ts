b923bd7b86768f95d1b0c4976f0ca945ec8939f2
Rohan Bansal <rohanbansal33@gmail.com>
2026-06-01 10:52:21 +0530

    chore: move graphql_middleware package from eslint to oxlint

--- graphql_middleware/oxlint.config.ts
+++ graphql_middleware/oxlint.config.ts
@@ -6,9 +6,11 @@ export default defineConfig({
 	extends: [root],
 	ignorePatterns: ['tests/**', 'eslint.config.js'],
 	rules: {
-		'typescript/no-unsafe-member-access': 'off',
-		'typescript/no-unsafe-argument': 'off',
-		'typescript/no-unsafe-call': 'off',
-		'typescript/require-await': 'off',
+		'no-console': 'error',
+		'no-underscore-dangle': [
+			'error',
+			{ allow: ['__dirname', '__filename', '__v_isReactive', '__caslSubjectType__', '_attributeName'] },
+		],
+		'typescript/no-unsafe-type-assertion': 'error',
 	},
 })
